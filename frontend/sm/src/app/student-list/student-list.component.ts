8d1c16175957a4b7902e531a9c3df85186697dbf
Ezhil R <66123055+Ezhil2389@users.noreply.github.com>
2023-07-25 12:37:23 +0530

    Added table fetch

--- frontend/sm/src/app/student-list/student-list.component.ts
+++ frontend/sm/src/app/student-list/student-list.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../student.model';
 
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
   styleUrls: ['./student-list.component.css']
 })
+
+
 export class StudentListComponent {
   
   showTable: boolean = false;
   searchQuery = '';
+  students: Student[] = []; // Update the type to Student[]
+  constructor(private http: HttpClient) {}
+
   onViewStudentsClick(): void {
-    // Here, you can fetch the student data from your backend service using HTTP requests.
-    // For demonstration purposes, let's assume we have the data in an array called "students".
-    // Replace this with your actual data fetching logic from the backend.
-    // For example, this.students = this.backendService.getStudents();
+    this.http.get<any[]>('http://your-backend-api-url/students').subscribe(
+      (response: any[]) => {
+        this.students = response; // Store the fetched student data in the 'students' array
+        this.showTable = true; // Show the table after data is fetched
+      },
+      (error: any) => {
+        console.error('Error fetching students:', error);
+        // You can handle the error here, e.g., display an error message to the user
+      }
+    );
     this.showTable = true;
   }
 
